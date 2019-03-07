954e3751d141b20dd1022ad25499efa3b81207b2
unknown <tpgml944@wku.ac.kr>
2019-03-07 22:50:43 +0900

    heroku

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gdruw33!60xq23efi)9pkamgs5j6ihx!xrzovq+dlab)d)r^cq'
+#SECRET_KEY = 'gdruw33!60xq23efi)9pkamgs5j6ihx!xrzovq+dlab)d)r^cq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
