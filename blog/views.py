c5a588b6afbeebe4cca5ec00e52a1c4f34f7de81
unknown <tpgml944@wku.ac.kr>
2019-03-07 21:25:28 +0900

    blog end

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,21 @@ from .models import Blog
 # Create your views here.
 def home(request):
     blogs = Blog.objects #쿼리셋
-    return render(request, 'home.html',{'blogs':blogs})
+    return render(request, 'blog/home.html',{'blogs':blogs})
 
 def detail(request, blog_id):
     details = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'detail.html',{'details':details})
+    return render(request, 'blog/detail.html',{'details':details})
+
+
+def new(request):
+    return render(request, 'blog/new.html')
+
+
+def create(request):
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/'+str(blog.id))
